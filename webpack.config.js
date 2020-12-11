aeda5dcef20e0ea3b26bdb3240f039707ec3a89b
Latch Jackn <latch.jack@gmail.com>
2020-12-11 11:14:04 +0000

    webpack config updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -22,7 +23,8 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           { loader: 'style-loader' },
-          { loader: 'css-loader', 
+          {
+            loader: 'css-loader',
             options: {
               importLoaders: 1,
               modules: {
@@ -30,12 +32,26 @@ module.exports = {
               }
             }
           },
-          { loader: 'postcss-loader', options: {
-            ident: 'postcss',
-            plugins: () => [autoprefixer()]
-          }}
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [autoprefixer()]
+            }
+          }
         ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        loader: 'url-loader?limit=8000&name=images/[name].[ext]'
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ]
 };
